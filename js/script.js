36c711028c34752f287d4b659da387891f067598
Xutrurn <kanjoskillz@gmail.com>
2021-02-02 10:44:26 +0100

    aggiunto caricamento user principale con js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
 // alert("Hello world");
 
+var app = new Vue({
+  el: '#app',
+  data: {
+    // oggetto user
+    user: {
+      name: "Nome Qualunque",
+      avatar: "img/avatar_5.jpg"
+    },
 
+
+
+
+  },
+
+  methods: {
+    saluta: function() {
+      return 'Ciao';
+    }
+  }
+
+});
 
 
 
